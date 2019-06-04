bb9d3fb13316d6379a020ac396ff736ef022e601
LuvForAirplanes <micah@abcrun.com>
2019-06-04 07:23:36 -0400

    Return a list of records by child.

---
+++ MSUDTrack/MSUDTrack.Services/RecordsService/DTOs/RecordsDTO.cs
@@ -0,0 +1,23 @@
+﻿using MSUDTrack.DataModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSUDTrack.Services.DTOs
+{
+    public class RecordsDTO
+    {
+        public List<PeriodDTO> Periods = new List<PeriodDTO>();
+    }
+
+    public class PeriodDTO
+    {
+       public  Period Period { get; set; }
+
+        public List<Record> Records { get; set; }
+            
+        /// Did you get your drone?
+        ///no... why? Just curious . thought maybe we could fly together sometime. That would be really cool! I actually thought about that. :)
+        ///I'd really enjoy it. Gotta run to a meeting now... ok, see you.
+    }
+}
---
+++ MSUDTrack/MSUDTrack.Services/RecordsService/RecordsService.cs
@@ -0,0 +1,39 @@
+﻿using MSUDTrack.DataModels.Models;
+using MSUDTrack.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSUDTrack.Services
+{
+    public class RecordsService : Repository<Record, string>
+    {
+        private readonly PeriodsService _periodsService;
+
+        public RecordsService(TrackerDbContext context, PeriodsService periodsService) : base(context)
+        {
+            _periodsService = periodsService;
+        }
+
+        public async Task<RecordsDTO> GetTodaysRecordsByChildAsync(string childId)
+        {
+            var periods = await _periodsService.ListAsync();
+            var report = new RecordsDTO();
+
+            foreach (var period in periods)
+            {
+                var records = Get().Where(r => r.ChildId == childId).Where(r => r.PeriodId == period.Id).ToList();
+
+                report.Periods.Add(new PeriodDTO()
+                {
+                    Period = period,
+                    Records = records
+                });
+            }
+
+            return report;
+        }
+    }
+}
