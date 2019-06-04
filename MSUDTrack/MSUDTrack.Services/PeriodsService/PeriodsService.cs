205fe7011bc503ddc03a3f9aae15b1e7e5c1569f
LuvForAirplanes <micah@abcrun.com>
2019-06-04 07:15:20 -0400

    Add PeriodsService.

---
+++ MSUDTrack/MSUDTrack.Services/PeriodsService/PeriodsService.cs
@@ -0,0 +1,12 @@
+﻿using MSUDTrack.DataModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSUDTrack.Services
+{
+    public class PeriodsService : Repository<Period, string>
+    {
+        public PeriodsService(TrackerDbContext dbContext) : base(dbContext) { }
+    }
+}
