1158133d6940b4a61a17b176229944384e014e88
LuvForAirplanes <micah@abcrun.com>
2019-06-05 06:39:50 -0400

    Add SolidMapper

---
+++ SolidMapper/SolidMapper/SolidMapper/IMapper.cs
@@ -0,0 +1,13 @@
+﻿namespace SolidMapper
+{
+    public interface IMapper<TDTO, TEntity>
+    {
+        TEntity Map(TDTO source);
+
+        TDTO Map(TEntity source);
+
+        void Map(TEntity source, TDTO target);
+
+        void Map(TDTO source, TEntity target);
+    }
+}
