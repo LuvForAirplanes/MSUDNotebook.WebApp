e15f184195e133dfeccff539dcb45f08acf318e1
LuvForAirplanes <micah@abcrun.com>
2019-06-20 09:40:13 -0400

    Reload page on food select change; wait till Ajax return to reload page.

--- MSUDTrack/MSUDTrack.WebApp/wwwroot/js/site.js
+++ MSUDTrack/MSUDTrack.WebApp/wwwroot/js/site.js
@@ -1,62 +1,66 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-$(".food-select").on("input", function (e) {
-    //here goes the api post
-    var targetId = $(this).attr('class').split(' ')[1];
-    var elements = $("." + targetId);
+$(document).ready(function () {
+    $(".food-select").on("input", function (e) {
+        //here goes the api post
+        var targetId = $(this).attr('class').split(' ')[1];
+        var elements = $("." + targetId);
 
-    var record = {
-        Id: targetId,
-        FoodId: elements[0].value
-    };
+        var record = {
+            Id: targetId,
+            FoodId: elements[0].value
+        };
 
-    $.ajax({
-        type: 'POST',
-        url: '/api/Records',
-        data: JSON.stringify(record),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json'
-    });
-});
-
-$(".nutri-inputs").on("input", function (e) {
-    //here goes the api post
-    var targetId = $(this).attr('class').split(' ')[1];
-    var elements = $("." + targetId);
-
-    var food = {
-        Id: elements[0].value,
-        ProteinGrams: elements[1].value,
-        LeucineMilligrams: elements[2].value
-    };
-
-    $.ajax({
-        type: 'POST',
-        url: '/api/Foods',
-        data: JSON.stringify(food),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json'
-    });
-});
-
-$(".delete-button").on("click", function (e) {
-    //here goes the api post
-    var targetId = $(this).attr('class').split(' ')[1];
-
-    $.ajax({
-        type: 'DELETE',
-        url: '/api/Records/' + targetId
+        $.ajax({
+            type: 'POST',
+            url: '/api/Records',
+            data: JSON.stringify(record),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'
+        }).done(function () {
+            location.reload();
+        });
     });
 
-    location.reload();
-});
+    $(".nutri-inputs").on("input", function (e) {
+        //here goes the api post
+        var targetId = $(this).attr('class').split(' ')[1];
+        var elements = $("." + targetId);
 
-$("#child-select").on("input", function (e) {
-    $.ajax({
-        type: 'GET',
-        url: '/api/Children/' + $('#' + e.target.id).find(":selected").text()
+        var food = {
+            Id: elements[0].value,
+            ProteinGrams: elements[1].value,
+            LeucineMilligrams: elements[2].value
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/Foods',
+            data: JSON.stringify(food),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'
+        });
     });
 
-    location.reload();
+    $(".delete-button").on("click", function (e) {
+        //here goes the api post
+        var targetId = $(this).attr('class').split(' ')[1];
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/Records/' + targetId
+        });
+
+        location.reload();
+    });
+
+    $("#child-select").on("input", function (e) {
+        $.ajax({
+            type: 'GET',
+            url: '/api/Children/' + $('#' + e.target.id).find(":selected").text()
+        }).done(function () {
+            location.reload();
+        });
+    });
 });
