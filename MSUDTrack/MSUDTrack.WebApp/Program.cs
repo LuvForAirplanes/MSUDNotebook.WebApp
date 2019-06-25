e72183405a913235fd14fc49cd249c6195e24ce4
LuvForAirplanes <micah@abcrun.com>
2019-06-25 17:00:51 -0400

    Listen on custom URLs.

--- MSUDTrack/MSUDTrack.WebApp/Program.cs
+++ MSUDTrack/MSUDTrack.WebApp/Program.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using MSUDTrack.Services;
 
 namespace MSUDTrack.WebApp
@@ -53,6 +58,7 @@ namespace MSUDTrack.WebApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:1000", "https://*:1234", "https://*:5000", "https://*", "http://*")
                 .UseStartup<Startup>();
     }
 }
